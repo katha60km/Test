535e42bd8af0b17fdc3014e1726d2884c28a67e8
katha@60k <ramukumarvsmi@gmail.com>
2024-03-07 10:10:52 +0530

    non: if,else statement

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-print(10+2*3**3)
+import math
+price = 100000
+has_good_credit = False
+
+if has_good_credit:
+    downpayment = 0.1 * price
+    
+else:
+    downpayment = 0.2 * price
+
+print(f'downpayment : {downpayment}')
